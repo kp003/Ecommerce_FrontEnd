bd9174c2ddad6e8e7c57cdb6839bba1ca2fd82d8
kp003 <khadkapawan111@gmail.com>
2021-08-06 01:07:29 -0500

    Updated Products page with product details

--- src/Component/product/Product.js
+++ src/Component/product/Product.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { isauthenticated } from '../backend/Auth';
+import { getAllProducts } from '../backend/ProductController';
 
 function Product() {
+    const [products,setProducts]=useState([]);
+    const user = isauthenticated();
+
+    const preload=()=>{
+        getAllProducts(user.message).then(res=>{
+            setProducts(res)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
+    useEffect(()=>{
+        preload();
+    },[])
     return (
-        <div>
-            This is the product's page
-        </div>
+        <Container fluid="md">
+            <Row xs={1} md={2} className="g-4">
+  {products.map((product) => (
+    <Col>
+      <Card>
+        <Card.Img variant="top" src={product.imageURL} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>
+            {product.description}
+          </Card.Text>
+          <div className="product_price">
+              <h1>${product.price}</h1>
+              <Button variant="outline-info">Add to cart</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
+  ))}
+</Row>
+
+
+        </Container>
     )
 }
 
